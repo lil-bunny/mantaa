c155e9e67651ac42e5302ffe42daa5fe1aa3b7c5
subhajit <subhajit.mukhejee@indusnet.co.in>
2023-01-19 15:08:12 +0530

    front end template integration

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,7 +13,7 @@
         </h1>
         <div class="container">
             <div class="search-widget">
-                <form  method="post" action="search.html">
+                <form  method="post" action="">
                     <input type="text" placeholder="Enter a location. Eg. City. locality or landmark"/>
                     <button class="btn btn-search">Search</button>
                 </form>
@@ -30,7 +30,67 @@
         <div class="row sites-items">
             <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
                 <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
+                    <a href="javascript:void(0);" class="d-block img-elm">
+                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
+                    </a>
+                    <div class="info-elmnt">
+                        <h3><a href="javascript:void(0);">Cras eu nulla sed tellus</a></h3>
+                        <h4>Etiam viverra auctor</h4>
+                    </div>
+                    <div class="bottom-widget d-flex justify-content-between align-items-center">
+                        <h6 class="mb-0">Starting from</h6>
+                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
+                <div class="site-box">
+                    <a href="javascript:void(0);" class="d-block img-elm">
+                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
+                    </a>
+                    <div class="info-elmnt">
+                        <h3><a href="javascript:void(0);">Cras eu nulla sed tellus</a></h3>
+                        <h4>Etiam viverra auctor</h4>
+                    </div>
+                    <div class="bottom-widget d-flex justify-content-between align-items-center">
+                        <h6 class="mb-0">Starting from</h6>
+                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
+                <div class="site-box">
+                    <a href="javascript:void(0);" class="d-block img-elm">
+                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
+                    </a>
+                    <div class="info-elmnt">
+                        <h3><a href="javascript:void(0);">Cras eu nulla sed tellus</a></h3>
+                        <h4>Etiam viverra auctor</h4>
+                    </div>
+                    <div class="bottom-widget d-flex justify-content-between align-items-center">
+                        <h6 class="mb-0">Starting from</h6>
+                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
+                <div class="site-box">
+                    <a href="javascript:void(0);" class="d-block img-elm">
+                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
+                    </a>
+                    <div class="info-elmnt">
+                        <h3><a href="javascript:void(0);">Cras eu nulla sed tellus</a></h3>
+                        <h4>Etiam viverra auctor</h4>
+                    </div>
+                    <div class="bottom-widget d-flex justify-content-between align-items-center">
+                        <h6 class="mb-0">Starting from</h6>
+                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
+                <div class="site-box">
+                    <a href="javascript:void(0);" class="d-block img-elm">
                         <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
                     </a>
                     <div class="info-elmnt">
@@ -45,11 +105,11 @@
             </div>
             <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
                 <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
+                    <a href="javascript:void(0);" class="d-block img-elm">
                         <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
                     </a>
                     <div class="info-elmnt">
-                        <h3><a href="plot-details.html">Cras eu nulla sed tellus</a></h3>
+                        <h3><a href="javascript:void(0);">Cras eu nulla sed tellus</a></h3>
                         <h4>Etiam viverra auctor</h4>
                     </div>
                     <div class="bottom-widget d-flex justify-content-between align-items-center">
@@ -64,7 +124,7 @@
                         <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
                     </a>
                     <div class="info-elmnt">
-                        <h3><a href="plot-details.html">Cras eu nulla sed tellus</a></h3>
+                        <h3><a href="javascript:void(0);">Cras eu nulla sed tellus</a></h3>
                         <h4>Etiam viverra auctor</h4>
                     </div>
                     <div class="bottom-widget d-flex justify-content-between align-items-center">
@@ -75,67 +135,7 @@
             </div>
             <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
                 <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
-                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
-                    </a>
-                    <div class="info-elmnt">
-                        <h3><a href="plot-details.html">Cras eu nulla sed tellus</a></h3>
-                        <h4>Etiam viverra auctor</h4>
-                    </div>
-                    <div class="bottom-widget d-flex justify-content-between align-items-center">
-                        <h6 class="mb-0">Starting from</h6>
-                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
-                <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
-                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
-                    </a>
-                    <div class="info-elmnt">
-                        <h3><a href="plot-details.html">Cras eu nulla sed tellus</a></h3>
-                        <h4>Etiam viverra auctor</h4>
-                    </div>
-                    <div class="bottom-widget d-flex justify-content-between align-items-center">
-                        <h6 class="mb-0">Starting from</h6>
-                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
-                <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
-                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
-                    </a>
-                    <div class="info-elmnt">
-                        <h3><a href="plot-details.html">Cras eu nulla sed tellus</a></h3>
-                        <h4>Etiam viverra auctor</h4>
-                    </div>
-                    <div class="bottom-widget d-flex justify-content-between align-items-center">
-                        <h6 class="mb-0">Starting from</h6>
-                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
-                <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
-                        <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
-                    </a>
-                    <div class="info-elmnt">
-                        <h3><a href="plot-details.html">Cras eu nulla sed tellus</a></h3>
-                        <h4>Etiam viverra auctor</h4>
-                    </div>
-                    <div class="bottom-widget d-flex justify-content-between align-items-center">
-                        <h6 class="mb-0">Starting from</h6>
-                        <h5 class="mb-0"><span class="currency">&#x20B9;</span> 4950.00</h5>
-                    </div>
-                </div>
-            </div>
-            <div class="site-item d-flex col-md-6 col-lg-3 col-xxl-3 col-xs-cs-6">
-                <div class="site-box">
-                    <a href="plot-details.html" class="d-block img-elm">
+                    <a href="javascript:void(0);" class="d-block img-elm">
                         <span class="d-block"><img src="{{asset('front-assets/images/sites-img.jpg') }}" alt="img"/></span>
                     </a>
                     <div class="info-elmnt">
@@ -149,7 +149,7 @@
                 </div>
             </div>
         </div>
-        <div class="text-center mt-5"><a href="#" class="btn btn-primary btn-more">View More<svg id="right-circle-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path id="Left_Arrow_5_" d="M6.5,12.5a.5.5,0,0,0,.354-.146l4-4a.5.5,0,0,0,0-.707l-4-4a.5.5,0,1,0-.707.707L9.793,8,6.147,11.646A.5.5,0,0,0,6.5,12.5ZM0,8a8,8,0,1,1,8,8A8.009,8.009,0,0,1,0,8ZM1,8A7,7,0,1,0,8,1,7.008,7.008,0,0,0,1,8Z" fill="#fff"/></svg></a></div>
+        <div class="text-center mt-5"><a href="javascript:void(0);" class="btn btn-primary btn-more">View More<svg id="right-circle-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path id="Left_Arrow_5_" d="M6.5,12.5a.5.5,0,0,0,.354-.146l4-4a.5.5,0,0,0,0-.707l-4-4a.5.5,0,1,0-.707.707L9.793,8,6.147,11.646A.5.5,0,0,0,6.5,12.5ZM0,8a8,8,0,1,1,8,8A8.009,8.009,0,0,1,0,8ZM1,8A7,7,0,1,0,8,1,7.008,7.008,0,0,0,1,8Z" fill="#fff"/></svg></a></div>
     </div>	
 </section>	
 <!-- // END SITES DEMAND -->		
