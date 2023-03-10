43fb1a2a636a484bbf2c43bd636214136321c718
subhajit <subhajit.mukhejee@indusnet.co.in>
2023-03-10 15:06:51 +0530

    dashboard section

--- Modules/Admin/Resources/views/dashboard/index.blade.php
+++ Modules/Admin/Resources/views/dashboard/index.blade.php
@@ -16,13 +16,12 @@
                             <div class="card padding-4 animate fadeLeft">
                                 <div class="row">
                                     <div class="col s5 m5">
-                                        <h5 class="mb-0">5</h5>
+                                        <h5 class="mb-0">{{ $active_user_count }}</h5>
                                         <p class="no-margin">Active</p>
-                                        <p class="mb-0 pt-8">50</p>
+                                        <p class="mb-0 pt-8">{{ $total_user_count }}</p>
                                     </div>
                                     <div class="col s7 m7 right-align">
-                                        <i
-                                            class="material-icons background-round mt-5 mb-5 gradient-45deg-purple-amber gradient-shadow white-text">perm_identity</i>
+                                        <i class="material-icons background-round mt-5 mb-5 gradient-45deg-purple-amber gradient-shadow white-text">perm_identity</i>
                                         <p class="mb-0">Total Users</p>
                                     </div>
                                 </div>
@@ -33,9 +32,9 @@
                                 <div class="card padding-4 animate fadeLeft">
                                     <div class="row">
                                         <div class="col s5 m5">
-                                            <h5 class="mb-0">3</h5>
+                                            <h5 class="mb-0">{{ $active_area_count }}</h5>
                                             <p class="no-margin">Active</p>
-                                            <p class="mb-0 pt-8">20</p>
+                                            <p class="mb-0 pt-8">{{ $total_area_count }}</p>
                                         </div>
                                         <div class="col s7 m7 right-align">
                                             <i
@@ -51,14 +50,12 @@
                                 <div class="card padding-4 animate fadeLeft">
                                     <div class="row">
                                         <div class="col s5 m5">
-                                            <h5 class="mb-0">3</h5>
+                                            <h5 class="mb-0">{{ $connect_request_count }}</h5>
                                             <p class="no-margin">Active</p>
-                                            <p class="mb-0 pt-8">20</p>
                                         </div>
                                         <div class="col s7 m7 right-align">
-                                            <i
-                                                class="material-icons background-round mt-5 mb-5 gradient-45deg-purple-amber gradient-shadow white-text">perm_identity</i>
-                                            <p class="mb-0">Advertisements</p>
+                                            <i class="material-icons background-round mt-5 mb-5 gradient-45deg-purple-amber gradient-shadow white-text">perm_identity</i>
+                                            <p class="mb-0">Connect Requests</p>
                                         </div>
                                     </div>
                                 </div>
@@ -74,32 +71,50 @@
                                     <div class="card">
                                     
                                             <!-- <div class="card-content pb-1"> -->
-                                            <h4 class="card-title mb-0">Recent Advertisements</h4>
+                                            <h4 class="card-title mb-0">Recent Onboarded Sites</h4>
                                             
                                             <!-- datatable start -->
                                             <div class="responsive-table">
                                                 <table id="users-list-datatable" class="table">
                                                     <thead>
                                                         <tr>
-                                                            <th>Serial No</th>
+                                                            <th>ID</th>
                                                             <th>Date & Time</th>
                                                             <th>Title</th>
+                                                            <th>Site Location</th>
+                                                            <th>Pincode</th>
                                                             <th>City</th>
+                                                            <th>State</th>
                                                             <th>Status</th>
                                                             <th>Actions</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
+                                                        @foreach($area_data as $area)
                                                         <tr>
-                                                            <th>1</th>
-                                                            <td>09/08/2021 08:00 PM</td>
-                                                            <td>Lorem Ipsum</td>
-                                                            <td>lorem Ipsum</td>
-                                                            <td>Pending</td>
-                                                            <td>    
-                                                                <a class="waves-effect waves-light modal-trigger" href="#modal1"><i class="material-icons red-text">delete</i></a>
+                                                            <th>{{ $area->id }}</th>
+                                                            <td>{{ $area->created_at }}</td>
+                                                            <td>{{ $area->title }}</td>
+                                                            <td>{{ $area->site_location }}</td>
+                                                            <td>{{ $area->pin_code }}</td>
+                                                            <td>{{ $area->city->name }}</td>
+                                                            <td>{{ $area->state->name }}</td>
+                                                            <td>
+                                                                @if($area->status == 1)
+                                                                    <span class="chip green lighten-5">
+                                                                        <span class="green-text">Active</span>
+                                                                    </span>
+                                                                @else
+                                                                    <span class="chip green lighten-5">
+                                                                        <span class="green-text">Inactive</span>
+                                                                    </span>
+                                                                @endif
+                                                            </td>    
+                                                            <td>
+                                                                <a href="{{ route('admin.area_edit', ['id' => $area->id]) }}"  title="Edit Area"><i class="material-icons">edit</i></a>
                                                             </td>
                                                         </tr>
+                                                        @endforeach
                                                     </tbody>
                                                 </table>
                                             </div>
