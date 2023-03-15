3883d613cb0fabab6f3efb574be7cd186a3c0899
Tanmoy Dutta <tanmoy.dutta@indusnet.co.in>
2023-03-15 12:12:07 +0530

    role id added on user

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -22,6 +23,15 @@ class GoogleController extends Controller
             $user = Socialite::driver('google')->user();
             
             $is_user = User::where('email', $user->getEmail())->first();
+
+            // fetching roles for customer
+            $roles = Role::where('is_deleted', '=', 0)
+                            ->where('status', '=', 1)
+                            ->where('role_id', '=', 'customer')
+                            ->get();
+            foreach($roles as $role) {
+                $role_id = $role->id;
+            }
             
             if(!$is_user){
                 $saveUser = User::updateOrCreate([
@@ -29,7 +39,8 @@ class GoogleController extends Controller
                 ],[
                     'full_name' => $user->getName(),
                     'email' => $user->getEmail(),
-                    'password' => Hash::make($user->getName().'@'.$user->getId())
+                    'password' => Hash::make($user->getName().'@'.$user->getId()),
+                    'role_id' => $role_id
                 ]);
             }else{
                 $saveUser = User::where('email',  $user->getEmail())->update([
