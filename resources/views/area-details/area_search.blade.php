d84f74d2f223c2d20c6f7274e7a102bb4b347642
subhajit <subhajit.mukhejee@indusnet.co.in>
2023-05-11 18:20:04 +0530

    js sanitization

--- resources/views/area-details/area_search.blade.php
+++ resources/views/area-details/area_search.blade.php
@@ -127,9 +127,6 @@
 <div id="location_result" style="display:none">{{$locations}}</div>
 <!-- ens search result -->
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css" />
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script
 src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}&callback=initMap"
 defer
