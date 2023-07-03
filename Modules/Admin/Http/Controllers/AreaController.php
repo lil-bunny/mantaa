31fa41e5ace75a2b82cb57e279b672649ba9cbb3
subhajit mukherjee <subhajit.mukherjee@indusnet.co.in>
2023-07-03 07:48:37 +0000

    area onboarding fixed

--- Modules/Admin/Http/Controllers/AreaController.php
+++ Modules/Admin/Http/Controllers/AreaController.php
@@ -323,11 +323,9 @@ class AreaController extends Controller
             // redirecting to the fetch poi section
             return redirect()->route('admin.fetch_poi', ['id' => $areas->id]);
         } else {
-            $request->request->remove('area_pic1');
-            $request->request->remove('area_pic2');
-            $request->request->remove('area_video');
+            $dt = $request->except(['area_pic1', 'area_pic2', 'area_video']);
             $errors=$validator->errors();
-            return redirect()->route('admin.area_add')->with('errors',$errors)->with('requestInput',request()->all());
+            return redirect()->route('admin.area_add')->with('errors',$errors)->with('requestInput',$dt);
         }
 
         return redirect()->intended('admin/areas')->withSuccess('Area created successfully');
