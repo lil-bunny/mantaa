f718901004ad81077e25b1271b97ea449e76ed00
subhajit <subhajit.mukhejee@indusnet.co.in>
2023-08-08 16:54:05 +0530

    sign up link added

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -33,7 +33,7 @@
             <button class="btn btn-primary btn-submit w-100" type="submit">Login</button>
             <div class="d-flex justify-content-between flex-wrap-xs">
                 <p class="text-muted mt-3 mb-0 w-100-xs"><a href="{{ route('frontend.showForgetPasswordForm') }}">Forgot password?</a></p>
-                <p class="text-muted mt-sm-3 w-100-xs">Don't have an account? <a href="javascript:void(0)">Sign up</a></p>
+                <p class="text-muted mt-sm-3 w-100-xs">Don't have an account? <a href="{{ route('frontend.register') }}">Sign up</a></p>
             </div>
         </form>
         <div class="mt-2 mt-sm-5 or-text"><span>OR</span></div>
